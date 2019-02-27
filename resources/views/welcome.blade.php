39f2d4b870ebbd14e851b4d84e6525c8cc930cd9
fireeagle25 <nechaev.p.v.25@gmail.com>
2019-02-27 22:40:37 +1000

    Jivo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,16 @@
 
     <script src="/Home_files/jquery.js"></script>
     <script src="/Home_files/jquery.maskedinput.min.js"></script>
+    <!-- BEGIN JIVOSITE CODE {literal} -->
+    <script type='text/javascript'>
+        (function(){ var widget_id = 'vaxM51Gk77';var d=document;var w=window;function l(){
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+            s.src = '//code.jivosite.com/script/widget/'+widget_id
+            ; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}
+            if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}
+            else{w.addEventListener('load',l,false);}}})();
+    </script>
+    <!-- {/literal} END JIVOSITE CODE -->
 </head>
 
 <body class="">
