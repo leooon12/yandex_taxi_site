1a397a906feffb1dc3584ba276937f8781925be1
fireeagle25 <nechaev.p.v.25@gmail.com>
2019-03-30 16:41:13 +1000

    смс-ки

---
+++ app/Jobs/BaseSms.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Zelenin\SmsRu\Api;
+use Zelenin\SmsRu\Auth\ApiIdAuth;
+use Zelenin\SmsRu\Entity\Sms;
+
+class BaseSms implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    protected $phoneNumber;
+    protected $text;
+    public function __construct($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws \Zelenin\SmsRu\Exception\Exception
+     */
+    public function handle()
+    {
+        $client = new Api(new ApiIdAuth(config('sms_service.api_id')));
+        $sms = new Sms($this->phoneNumber, $this->text);
+        $client->smsSend($sms);
+    }
+}
