ea0638d455c5121a92ef308345bd6aed41b716c7
fireeagle25 <nechaev.p.v.25@gmail.com>
2019-03-30 17:03:36 +1000

    отправка sms при регистрации

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRegisterRequest;
+use App\Jobs\SendRegistrationSms;
 use App\UserJWT;
 use Illuminate\Http\Request;
 use JWTAuth;
@@ -13,13 +14,19 @@ class UserController extends Controller
 
     public function register(UserRegisterRequest $request)
     {
+
+        $code = (string)(rand(100000, 999999));
+
         $user = UserJWT::create([
             'name' => $request->get('name'),
             'phone_number' => $request->get('phone_number'),
-            'password' => bcrypt('000000'),
+            'password' => bcrypt($code),
         ]);
 
         $token = JWTAuth::fromUser($user);
+
+        $regSms = new SendRegistrationSms($request->get('phone_number'), $code);
+        $this->dispatch($regSms);
 
         return ResponseHandler::getJsonResponse(200, "Вы успешно зарегистрированы, ожидайте смс-пароля.", compact('user', 'token'));
     }
