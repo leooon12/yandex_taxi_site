94f7126670a7fbd68db2a26c3c53c5b8693d7555
fireeagle25 <nechaev.p.v.25@gmail.com>
2019-06-15 16:00:17 +1000

    Роут для тестов

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+use App\AnotherClasses\Builders\DriverInfo;
+use App\AnotherClasses\TaximeterConnector;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,4 +26,34 @@ Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
     Route::get('withdrawal/','AdminPanelWithdrawalController@index');
     Route::get('edit_request/','AdminRequestController@index');
+});
+
+
+Route::get('/test', function () {
+    $driverInfo = new DriverInfo();
+
+    $driverInfo->setName("Леонид")
+        ->setSurname("Бабков")
+        ->setPatronymic("Николаевич")
+        ->setBirthdate("1990-09-09")
+        ->setPhone("79145551353");
+
+    $driverInfo->getCarInfo()
+        ->setBrand("AC")
+        ->setModel("378%20GT%20ZAGATO")
+        ->setGovNumber("x000xx125")
+        ->setColor("Серый")
+        ->setVin("11111111111111111")
+        ->setCreationYear("1991")
+        ->setRegSertificate("2813456789");
+
+    $driverInfo->getDriverDocumentInfo()
+        ->setSerialNumber("0123")
+        ->setUniqNumber("456788")
+        ->setCountry("rus")
+        ->setEndDate("2029-09-09")
+        ->setIssueDate("2001-09-09");
+
+    $driverCreationResult = TaximeterConnector::createDriver($driverInfo);
+    dump($driverCreationResult);
 });
