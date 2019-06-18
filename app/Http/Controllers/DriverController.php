3d2f4b6b1634bbaa2ea51f116b2e2f00d32c5076
fireeagle25 <nechaev.p.v.25@gmail.com>
2019-06-19 00:53:39 +1000

    Отмена теста

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -71,7 +71,7 @@ class DriverController extends Controller
         $editResponce = TaximeterConnector::editDriver($driverInfo);
 
         if (!isset($editResponce['redirect']))
-            return ResponseHandler::getJsonResponse(500, "Номер телефона не изменен", compact('editResponce', 'driverInfo'));
+            return ResponseHandler::getJsonResponse(500, "Номер телефона не изменен", compact('editResponce'));
 
         $user = UserJWT::where('id', $user_id)
             ->update([
