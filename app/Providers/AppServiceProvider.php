97507e6e411ff2e678fb8f71e6fcb9ce26ca16c5
develeone <babkov@awake.su>
2019-06-18 23:19:29 +1000

    Headers Control-Allow, isset fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+	    header('Access-Control-Allow-Origin: *');
+	    header("Access-Control-Allow-Headers: *");
+	    
         Schema::defaultStringLength(191);
     }
 
