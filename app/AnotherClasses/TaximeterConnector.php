625c63be4ea2ec7a29648c440015b3f28bd218a1
dmitry <adgfq121519@gmail.com>
2020-04-03 01:35:53 +1000

    ``

--- app/AnotherClasses/TaximeterConnector.php
+++ app/AnotherClasses/TaximeterConnector.php
@@ -205,7 +205,7 @@ class TaximeterConnector
 
         $postfields = '{'.
             '"park_id":"' . TaximeterConnector::PARK_ID . '",'.
-            '"text":"' . substr($phonenumber, 1, 10) . '"'.
+            '"text":"' . substr($phonenumber, 2, 10) . '"'.
         '}';
 
         $driversData = TaximeterConnector::newPost($url, $postfields);
